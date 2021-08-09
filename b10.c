1b7f6829c8fb208587fcc4da657d4c97bbce3e78
hanhybh <88534923+hanhybh@users.noreply.github.com>
2021-08-09 22:34:50 +0700

    Update_b10.c

--- b10.c
+++ b10.c
@@ -1,29 +1,31 @@
-#include<conio.h>
 #include<stdio.h>
+
+void nhap(int *a,int n) {
+    printf("nhap mang :");
+          for(int i=0;i<n;i++) {
+                printf("a[%d]=",i);
+                scanf("%d",(a+i));
+          }
+}
+
+void xuat(int *a,int n) {
+    printf("xuat mang :");
+          for(int i=0;i<n;i++) {
+                printf("a[%d]=%d",i,*(a+i));
+          }
+}
+
 int main() {
-    int n;
-    int X=5;
-    float A=1;
-    //printf("nhap x = ");
-    //scanf("%d",&x);
-    printf("nhap n = ");
+    int *a;
+    int  n;
+    a=(int*)malloc(n*size);
+    printf("nhap n =  ");
     scanf("%d",&n);
     
-    if(n > 0) {
-        for(int i=1;i<=n;i++) {
-            A=A*X;
-        }
-        printf("A = %f",A);
-        printf("\n n = %d",n);
-    }
-    else {
-        n=-n;
-        for(int i=1;i<=n;i++) {
-            A=A*1.0/X;
-        }
-        printf("A = %f",A);
-        printf("\n X = %d",n);
-    }
-    //printf("A = %f",A);
-    return 0;
+    nhap(a,n);
+    xuat(a,n);
+    
+    return(0);
 }
+   
+    
